13998e217df802590d1775243fc15fd44118d3c6
anish2210 <anishjaiswal1220@gmail.com>
2023-06-29 17:53:42 +0530

    Introduced CRUD operations

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://gofood:Anishgofood@cluster0.uzlrntf.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://gofood:Anishgofood@cluster0.uzlrntf.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 const mongoDB =async() =>{
-    await mongoose.connect(mongoURI,{useNewUrlParser: true},(err, result)=> {
+    await mongoose.connect(mongoURI,{useNewUrlParser: true},async(err, result)=> {
         if(err)console.log("---",err)
         else{
         console.log("connected");
+        const fetched_data = await mongoose.connection.db.collection("food_items");
+        fetched_data.find({}).toArray(function(err, data){
+            if(err) console.log(err);
+            // else console.log(data);
+        })
         }
     });
 }
