f0a03ec71f9474f20a9adaf19833e6e32f1863ba
anish2210 <anishjaiswal1220@gmail.com>
2023-07-09 02:04:07 +0530

    additional changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,20 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
+
+    let options = props.options;
+    let priceOptions = Object.keys(options);
+
+
+
     return (
         <div>
             <div>
                 <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                    <img src="https://images.unsplash.com/photo-1546069901-eacef0df6022?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80" className="card-img-top" alt="..." />
+                    <img src={props.imgSrc}  className="card-img-top" alt="..."style={{height:"140px", objectFit:"fill"}}/>
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is some important text.</p>
+                        <h5 className="card-title">{props.foodName}</h5>
+                
                         <div className="container w - 100">
                             <select className="m-2 h-100 bg-success rounded">
                                 {Array.from(Array(6), (e, i) => {
@@ -19,9 +25,9 @@ export default function Card() {
                             </select>
 
                             <select className="m-2 h-100 bg-success rounded">
-                                <option value="half">Half</option>
-                                <option value="full">Full</option>
-
+                                {priceOptions.map((data)=>{
+                                    return <option key={data} value={data}>{data}</option>
+                                })}
                             </select>
 
                             <div className='d-inline h-100 fs-5'>
