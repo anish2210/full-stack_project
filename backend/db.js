22a43234097e728b2f6d880a63c0e14dd70bebef
anish2210 <anishjaiswal1220@gmail.com>
2023-07-08 19:30:47 +0530

    received all data in the frontend

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb+srv://gofood:Anishgofood@cluster0.uzlrntf.mongodb.net/gofoodmern?retryWrites=true&w=majority'
-const mongoDB =async() =>{
-    await mongoose.connect(mongoURI,{useNewUrlParser: true},async(err, result)=> {
-        if(err)console.log("---",err)
-        else{
-        console.log("connected");
-        const fetched_data = await mongoose.connection.db.collection("food_items");
-        fetched_data.find({}).toArray(function(err, data){
-            if(err) console.log(err);
-            // else console.log(data);
-        })
+const mongoDB = async () => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        if (err) console.log("---", err)
+        else {
+            console.log("connected");
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(async function (err, data) {
+                const foodCategory = await mongoose.connection.db.collection("foodCategory");
+                foodCategory.find({}).toArray(function (err, catData) {
+                    
+                    if (err) console.log(err);
+                    else {
+                        global.food_items = data;                        
+                        global.foodCategory = catData;}                       
+                    })
+                // if (err) console.log(err);
+                // else {
+                //     global.food_items = data;
+                // }
+            })
         }
     });
 }
